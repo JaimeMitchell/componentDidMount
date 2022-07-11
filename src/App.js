59b82c0db113fb8155b8dd685a1948b58c6f3c18
Jaime Mitchell <JaimeMitchell100@gmail.com>
2022-07-11 18:28:38 -0400

    loading files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  // constructor(){
+  //   super()
+  //   this.state ={
+  //     giphy: []
+  //   }
+  // }
+  state = {
+    giffy: []
+  }
+
+  componentDidMount() {
+    fetch('https://api.giphy.com/v1/gifs/trending?api_key=sm1t4ygsjFoVmIrfFipykq7h1NCdWVCI&limit=25&rating=g')
+      .then(res => res.json())
+      .then(getGiffy => {
+        console.log(getGiffy.data)
+        this.setState({ giffy: getGiffy.data })
+      })
+  }
+  render() {
+    return (
+      <div className='App'>
+        {this.state.giffy.map(g =>
+          <div> <img src={g.images.original.url} />
+            <h3>{g.title}</h3>
+          </div>)}
+
+      </div>
+    )
+  }
 }
-
 export default App;
