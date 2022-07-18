d4315972145db271cad05906d7878f3a8ba4f61e
Jaime Mitchell <JaimeMitchell100@gmail.com>
2022-07-18 16:02:09 -0400

    Please enter the commit message for your changes.  finito

---
+++ src/groceryData.js
@@ -0,0 +1,24 @@
+const GroceryData = [
+    {
+        Item: "carrots",
+        Brand: "monsatan",
+        Units: "yum",
+        Quantity: 5,
+        IsPurchased: false
+    },
+    {
+        Item: "gnocci",
+        Brand: "TJs",
+        Units: "100",
+        Quantity: 100,
+        IsPurchased: true
+    },
+    {
+        Item: "dark chocolate",
+        Brand: "yum",
+        Units: "20lbs",
+        Quantity: 20,
+        IsPurchased: false
+    }
+];
+export default GroceryData
